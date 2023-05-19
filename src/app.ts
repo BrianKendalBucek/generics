63a19bd3ca7d7872abf0e8843ee8b3b93b28ff06
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-18 20:15:48 -0700

    example of merged object

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-const names: Array<string> = [];
-// names[0].split('');
+// const names: Array<string> = [];
+// // names[0].split('');
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-  }, 2000);
-});
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
 
-promise.then(data => {
-  // data.split('');
-})
+// promise.then(data => {
+//   // data.split('');
+// })
+
+function merge(objA: object, objB: object) {
+  return Object.assign(objA, objB);
+}
+
+console.log(merge({name: 'Max'}, {age: 30}));
