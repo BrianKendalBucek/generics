557f4733d835ae3d7aeb55da5dd1daa089314055
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-18 20:38:42 -0700

    fixed udemy error with generics

--- src/app.ts
+++ src/app.ts
@@ -11,9 +11,10 @@
 //   // data.split('');
 // })
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U>(objA: T, objB: U): T & U {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge<{name: string, hobbies: string[]}, {age: number}>({name: 'Max', hobbies: ['Sports'] }, {age: 30});
+const mergedObj = merge({name: 'Max'}, {age: 30});
+
 console.log(mergedObj);
