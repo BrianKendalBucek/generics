1c0e301be496e07450291b3fa19cae37a2e713a1
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-19 19:50:47 -0700

    examples of type constraints

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,10 @@
 //   // data.split('');
 // })
 
-function merge<T extends object, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge({name: 'Max'}, {age: 30});
+const mergedObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30});
 
 console.log(mergedObj);
