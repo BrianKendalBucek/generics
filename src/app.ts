506ead010b815a0221461f4229c1ab92fba1c99a
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-19 20:15:24 -0700

    example of a generic class

--- src/app.ts
+++ src/app.ts
@@ -43,3 +43,19 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: "Max" }, "name");
+
+class DataStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
