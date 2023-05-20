d8ac1d921e3d6c0e4fd90d20eaf2ee3363307fa4
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-19 20:30:18 -0700

    working with objects in generic classes

--- src/app.ts
+++ src/app.ts
@@ -52,6 +52,9 @@ class DataStorage<T> {
   }
 
   removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
     this.data.splice(this.data.indexOf(item), 1);
   }
 
@@ -61,9 +64,16 @@ class DataStorage<T> {
 }
 
 const textStorage = new DataStorage<string>();
-textStorage.addItem('Max');
-textStorage.addItem('Manu');
-textStorage.removeItem('Max');
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
 console.log(textStorage.getItems());
 
 const numberStorage = new DataStorage<number>();
+
+const objStorage = new DataStorage<object>();
+const maxObj = {name: 'Max'};
+objStorage.addItem(maxObj);
+objStorage.addItem({ name: "Manu" });
+objStorage.removeItem(maxObj);
+console.log(objStorage.getItems());
