8991a975f875c2998c11509cd4eeb4824f8e8c5c
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-19 20:34:54 -0700

    added type constraints

--- src/app.ts
+++ src/app.ts
@@ -44,14 +44,14 @@ function extractAndConvert<T extends object, U extends keyof T>(
 
 extractAndConvert({ name: "Max" }, "name");
 
-class DataStorage<T> {
+class DataStorage<T extends string | number | boolean> {
   private data: T[] = [];
 
   addItem(item: T) {
     this.data.push(item);
   }
 
-  removeItem(item: T) {
+  removeItem<U>(item: T) {
     if (this.data.indexOf(item) === -1) {
       return;
     }
@@ -71,9 +71,10 @@ console.log(textStorage.getItems());
 
 const numberStorage = new DataStorage<number>();
 
-const objStorage = new DataStorage<object>();
-const maxObj = {name: 'Max'};
-objStorage.addItem(maxObj);
-objStorage.addItem({ name: "Manu" });
-objStorage.removeItem(maxObj);
-console.log(objStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// const maxObj = { name: "Max" };
+// objStorage.addItem(maxObj);
+// objStorage.addItem({ name: "Manu" });
+// objStorage.removeItem(maxObj);
+// console.log(objStorage.getItems());
